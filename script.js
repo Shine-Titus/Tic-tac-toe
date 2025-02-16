70e0c3084cb144c62edcf74a1e388f760be91a8a
Shine-Titus <84309963+Shine-Titus@users.noreply.github.com>
2025-02-16 21:44:44 +0530

    modified js

--- script.js
+++ script.js
@@ -2,91 +2,44 @@ const boxes = document.querySelectorAll('button:not(#reset)');
 const msg = document.querySelector('span');
 const reset = document.getElementById('reset');
 let markX = true;
-let object = {1:'',2:'',3:'',4:'',5:'',6:'',7:'',8:'',9:''};
-let checkAdded = 0;
+let board = Array(9).fill('');
 
-function addCheck(box) {
+const winPatterns = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+    [0, 4, 8], [2, 4, 6]             // Diagonals
+];
 
-    if (msg.innerText == 'X goes first always!') {
-        if (markX && box.innerText == '') {
-            box.innerText = 'X';
-            object[box.value] = 'X';
-            markX = false;
-        };
-        if (!markX && box.innerText == '') {
-            box.innerText = 'O';
-            object[box.value] = 'O';
-            markX = true;
-        };
-        checkAdded += 1;
-    };
-};
+function addCheck(box, index) {
+    if (box.innerText || msg.innerText.includes("won") || msg.innerText === "It's a tie!") return;
+
+    board[index] = markX ? 'X' : 'O';
+    box.innerText = board[index];
+    markX = !markX;
+
+    checkGameStatus();
+}
 
 function checkGameStatus() {
-
-    if ((object[1] == object[3] && object[2] == object[3]) && object[1] != '')  {
-        msg.innerText = `${object[1]} won !!!`;
-        return;
-    }
-    if ((object[4] == object[6] && object[5] == object[6]) && object[4] != '') {
-        msg.innerText = `${object[4]} won !!!`;
-        return;
-    }
-    if ((object[7] == object[9] && object[8] == object[9]) && object[7] != '') {
-        msg.innerText = `${object[7]} won !!!`;
-        return;
+    for (const pattern of winPatterns) {
+        const [a, b, c] = pattern;
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+            msg.innerText = `${board[a]} won !!!`;
+            return;
+        }
     }
 
-    if ((object[1] == object[4] && object[4] == object[7]) && object[1] != '') {
-        msg.innerText = `${object[1]} won !!!`;
-        return;
-    }
-    if ((object[2] == object[5] && object[5] == object[8]) && object[2] != '') {
-        msg.innerText = `${object[2]} won !!!`;
-        return;
-    }
-    if ((object[3] == object[6] && object[6] == object[9]) && object [3] != '') {
-        msg.innerText = `${object[3]} won !!!`;
-        return;
-    }
-
-    if ((object[1] == object[5] && object[5] == object[9]) && object[1] != '') {
-        msg.innerText = `${object[1]} won !!!`;
-        return;
-    }
-    if ((object[3] == object[5] && object[5] == object[7])&& object[3] != '') {
-        msg.innerText = `${object[3]} won !!!`;
-        return;
+    if (board.every(cell => cell)) {
+        msg.innerText = "It's a tie!";
     }
 }
 
 function gameReset() {
-    if (msg.innerText != 'X goes first always!') {
-        object = {1:'',2:'',3:'',4:'',5:'',6:'',7:'',8:'',9:''};
-        msg.innerText = 'X goes first always!';
-        boxes.forEach(box => {
-        box.innerText = '';
-        });
-    } else {
-        alert('complete the game to reset.');
-    };
-};
+    board.fill('');
+    markX = true;
+    msg.innerText = 'X goes first always!';
+    boxes.forEach(box => box.innerText = '');
+}
 
-
-
-boxes.forEach(box => {
-
-    box.addEventListener('click', () => {
-        
-        addCheck(box);
-        checkGameStatus();
-
-        if (checkAdded == 9) {
-            msg.innerText = 'Its a tie!';
-        }
-
-    });
-
-});
-
+boxes.forEach((box, index) => box.addEventListener('click', () => addCheck(box, index)));
 reset.addEventListener('click', gameReset);
